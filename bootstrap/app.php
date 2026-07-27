03bf29900eee1dadd097aa8641e3bd6f66bf3ebf
Muhammad Haris <mh723595@gmail.com>
2026-07-27 09:57:03 +0700

    fix bug

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,14 +3,24 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+
+        $middleware->trustProxies(
+            at: '*',
+            headers: \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_FOR
+                | \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_HOST
+                | \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_PORT
+                | \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_PROTO
+        );
+
         $middleware->alias([
             'admin' => \App\Http\Middleware\EnsureUserIsAdmin::class,
         ]);
